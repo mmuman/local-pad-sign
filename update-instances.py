d48e648e6b99eaed020ac4c3c89f531a9d5939a5
François Revol <revol@free.fr>
2020-03-23 05:04:50 +0100

    Add a python helper to update etherpad instances from Framapad repo

---
+++ update-instances.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import json
+import yaml
+try:
+    from urllib import request
+except ImportError:
+    import urllib2 as request
+
+with request.urlopen("https://framagit.org/framasoft/framapad/-/raw/master/app/data/project.yml?inline=false") as stream:
+    #with open("project.yml", 'r') as stream:
+    data_loaded = yaml.safe_load(stream)
+    data = data_loaded['instances']
+    with open('js/instances.json', 'w') as outfile:
+        json.dump(data, outfile)
